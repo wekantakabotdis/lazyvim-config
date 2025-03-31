11d0612154c8f0ce30a9a8e7dd41c3e3b7313b82
wekantakabotdis <chenyiningchris@gmail.com>
2025-03-31 03:26:05 -0400

    PDF viewer

--- init.lua
+++ init.lua
@@ -1,2 +1,4 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.keymap.set({ "n" }, "<leader>ci", ":split term://ipython", { desc = "[c]ode repl [i]python" })
--- lua/plugins/R.nvim.lua
+++ lua/plugins/R.nvim.lua
@@ -2,4 +2,9 @@ return {
   "R-nvim/R.nvim",
   -- Only required if you also set defaults.lazy = true
   lazy = false,
+
+  opts = {
+    pdfviewer = "",
+    setwd = "nvim",
+  },
 }
