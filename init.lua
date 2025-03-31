386a51c4a428d8c82b6c05cfe3c5f36cfe5d9833
wekantakabotdis <chenyiningchris@gmail.com>
2025-03-31 04:15:20 -0400

    Quarto Shortcuts

--- init.lua
+++ init.lua
@@ -1,4 +1,14 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
 
-vim.keymap.set({ "n" }, "<leader>ci", ":split term://ipython", { desc = "[c]ode repl [i]python" })
+require("config.lazy")
+vim.keymap.set({ "n" }, "<leader>ci", ":split term://ipython<Enter>", { desc = "[c]ode repl [i]python" })
+
+local runner = require("quarto.runner")
+vim.keymap.set("n", "<localleader>rc", runner.run_cell, { desc = "run cell", silent = true })
+vim.keymap.set("n", "<localleader>ra", runner.run_above, { desc = "run cell and above", silent = true })
+vim.keymap.set("n", "<localleader>rA", runner.run_all, { desc = "run all cells", silent = true })
+vim.keymap.set("n", "<localleader>rl", runner.run_line, { desc = "run line", silent = true })
+vim.keymap.set("v", "<localleader>r", runner.run_range, { desc = "run visual range", silent = true })
+vim.keymap.set("n", "<localleader>RA", function()
+  runner.run_all(true)
+end, { desc = "run all cells of all languages", silent = true })
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,17 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set(
+  "n",
+  "<leader>ir",
+  "i```{r}<Esc>o<Enter>```<Esc>ki",
+  { noremap = true, silent = true, desc = "Insert R Code" }
+)
+
+vim.keymap.set(
+  "n",
+  "<leader>ip",
+  "i```{python}<Esc>o<Enter>```<Esc>ki",
+  { noremap = true, silent = true, desc = "Insert Python Code" }
+)
