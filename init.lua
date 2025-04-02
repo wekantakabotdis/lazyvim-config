b090217df0e404b914d2fd67d1e1e3f0738e556f
wekantakabotdis <chenyiningchris@gmail.com>
2025-04-02 01:35:28 -0400

    latex fix

--- init.lua
+++ init.lua
@@ -1,3 +1,10 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 
 require("config.lazy")
+
+require("nvim-treesitter.configs").setup({
+  highlight = {
+    enable = true,
+    disable = { "latex" }, -- Disable Tree-sitter highlighting for LaTeX files
+  },
+})
--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -4,6 +4,7 @@ return {
   -- tag = "v2.15", -- uncomment to pin to a specific release
   init = function()
     -- VimTeX configuration goes here, e.g.
-    vim.g.vimtex_view_method = "zathura"
+    vim.g.vimtex_view_method = "general"
+    vim.g.vimtex_syntax_enabled = 0
   end,
 }
