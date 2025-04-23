59f3977fcab695abed0f6f0bbd2e10b817c3424e
wekantakabotdis <chenyiningchris@gmail.com>
2025-04-23 16:33:05 -0400

    Oil Preview on startup

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,11 +1,30 @@
 return {
   "stevearc/oil.nvim",
-  ---@module 'oil'
-  ---@type oil.SetupOpts
-  opts = {},
-  -- Optional dependencies
-  dependencies = { { "echasnovski/mini.icons", opts = {} } },
-  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-  -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+  opts = {
+    keymaps = {
+      ["<C-h>"] = false, -- Disable Ctrl+h
+    },
+  },
+  config = function()
+    require("oil").setup({
+      keymaps = {
+        ["<C-h>"] = false, -- Disable Ctrl+h
+      },
+    })
+
+    -- Automatically open preview on Oil buffer enter
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "OilEnter",
+      callback = function(args)
+        local oil = require("oil")
+        if vim.api.nvim_get_current_buf() == args.data.buf and oil.get_cursor_entry() then
+          oil.open_preview()
+        end
+      end,
+    })
+  end,
+  dependencies = {
+    { "echasnovski/mini.icons", opts = {} },
+  },
   lazy = false,
 }
