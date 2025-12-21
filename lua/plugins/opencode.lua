6606ca81963ae14ed95595def1cf2331fe718253
wekantakabotdis <chenyiningchris@gmail.com>
2025-12-21 01:37:37 -0500

    Opencode

---
+++ lua/plugins/opencode.lua
@@ -0,0 +1,50 @@
+return {
+  "NickvanDyke/opencode.nvim",
+  dependencies = {
+    -- Recommended for ask() and select().
+    -- Required for snacks provider.
+    { "folke/snacks.nvim", opts = { input = {}, picker = {}, terminal = {} } },
+  },
+  config = function()
+    ---@type opencode.Opts
+    vim.g.opencode_opts = {
+      -- Default configuration - see :h opencode.config for more options
+    }
+
+    -- Required for opts.events.reload.
+    vim.o.autoread = true
+
+    -- Recommended/example keymaps.
+    vim.keymap.set({ "n", "x" }, "<C-a>", function()
+      require("opencode").ask("@this: ", { submit = true })
+    end, { desc = "Ask opencode" })
+
+    vim.keymap.set({ "n", "x" }, "<C-x>", function()
+      require("opencode").select()
+    end, { desc = "Execute opencode action…" })
+
+    vim.keymap.set({ "n", "t" }, "<C-.>", function()
+      require("opencode").toggle()
+    end, { desc = "Toggle opencode" })
+
+    vim.keymap.set({ "n", "x" }, "go", function()
+      return require("opencode").operator("@this ")
+    end, { expr = true, desc = "Add range to opencode" })
+
+    vim.keymap.set("n", "goo", function()
+      return require("opencode").operator("@this ") .. "_"
+    end, { expr = true, desc = "Add line to opencode" })
+
+    vim.keymap.set("n", "<S-C-u>", function()
+      require("opencode").command("session.half.page.up")
+    end, { desc = "opencode half page up" })
+
+    vim.keymap.set("n", "<S-C-d>", function()
+      require("opencode").command("session.half.page.down")
+    end, { desc = "opencode half page down" })
+
+    -- Remap increment/decrement since we're using <C-a> and <C-x> for opencode
+    vim.keymap.set("n", "+", "<C-a>", { desc = "Increment", noremap = true })
+    vim.keymap.set("n", "-", "<C-x>", { desc = "Decrement", noremap = true })
+  end,
+}
